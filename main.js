d3e152a760aff2f9cb2e78cca8e1afed88c4e19b
KangSukyu <77279949+KangSukyu@users.noreply.github.com>
2025-07-23 14:42:30 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,19 +1,18 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 
 app.get('/', (req, res) => {
-  const jwt = req.header('X-Goog-IAP-JWT-Assertion')
+  const jwt = req.header('X-Goog-IAP-JWT-Assertion');
   res.send(`
     <html>
       <body>
         <code>${jwt ? jwt : "No JWT header"}</code>
       </body>
     </html>
-  `)
-})
+  `);
+});
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
 });
-
